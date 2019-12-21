73dcdbd0c3650cd31c84bb4727a55301cfeac0d2
neil3d <neil3d@126.com>
2019-12-21 12:21:32 +0800

    plugin update

--- CleanProject.bat
+++ CleanProject.bat
@@ -2,5 +2,5 @@ RMDIR /S /Q Binaries
 RMDIR /S /Q Intermediate
 RMDIR /S /Q Saved
 
-RMDIR /S /Q Plugins\AnimatedTexture\Binaries
-RMDIR /S /Q Plugins\AnimatedTexture\Intermediate
+RMDIR /S /Q Plugins\AnimatedTexturePlugin\Binaries
+RMDIR /S /Q Plugins\AnimatedTexturePlugin\Intermediate
